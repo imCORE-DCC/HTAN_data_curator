0171a822a9f2eabf65484082336782335e5d932c
Bruno Grande <bruno.grande@sagebase.org>
2021-07-12 15:09:05 -0700

    Include script that created OAuth client

---
+++ create_oauth_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import synapseclient
+import json
+
+syn = synapseclient.login()
+
+client_meta_data = {
+    "client_name": "imCORE Data Curator",
+    "redirect_uris": [
+        "https://shinypro.synapse.org/users/bgrande/imcore_data_curator/"
+    ],
+    "sector_identifier_uri": "https://raw.githubusercontent.com/imCORE-DCC/data_curator/production/redirect_uris.json",
+    "userinfo_signed_response_alg": "RS256",
+}
+
+# Create the client:
+client_meta_data = syn.restPOST(
+    uri="/oauth2/client", endpoint=syn.authEndpoint, body=json.dumps(client_meta_data)
+)
+
+client_id = client_meta_data["client_id"]
+
+# Generate and retrieve the client secret:
+client_id_and_secret = syn.restPOST(
+    uri="/oauth2/client/secret/" + client_id, endpoint=syn.authEndpoint, body=""
+)
+
+print(client_id_and_secret)
