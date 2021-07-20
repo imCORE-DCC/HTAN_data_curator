7ce98d1eb3a952be17d6d341bd93abf15f4318ab
Bruno Grande <bruno.grande@sagebase.org>
2021-07-20 01:41:36 +0000

    Handle relative paths in schematic config

--- functions/metadata_model.py
+++ functions/metadata_model.py
@@ -1,13 +1,20 @@
+import os
+import yaml
 from schematic.models.metadata import MetadataModel
 from schematic import CONFIG
 
+with open("config.yaml") as app_config_file:
+    app_config = yaml.safe_load(app_config_file)
+    schematic_config = app_config["schematic_config"]
+    schematic_config_dir = os.path.dirname(schematic_config)
 
-config = CONFIG.load_config("/home/bgrande/repos/imcore_data_model/config.yml")
+config = CONFIG.load_config(app_config["schematic_config"])
 
 inputMModelLocation = CONFIG["model"]["input"]["location"]
 inputMModelLocationType = CONFIG["model"]["input"]["file_type"]
 
-manifest_title = CONFIG["manifest"]["title"]
-manifest_data_type = CONFIG["manifest"]["data_type"]
+if not os.path.isabs(inputMModelLocation):
+    inputMModelLocation = os.path.join(schematic_config_dir, inputMModelLocation)
+    inputMModelLocation = os.path.normpath(inputMModelLocation)
 
 metadata_model = MetadataModel(inputMModelLocation, inputMModelLocationType)
